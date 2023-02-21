65cdefd8619d13f2b9e267b5f0c6d749643c1b83
Emmanuel A <emmanuel.adekplovi@homescriptone.com>
2023-02-21 13:10:44 +0100

    Update woo-usn.php

--- woo-usn.php
+++ woo-usn.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Ultimate SMS & WhatsApp Notifications for WooCommerce
  * Plugin URI:        https://ultimatesmsnotifications.com?utm_source=customer_websites&utm_medium=plugin_page
  * Description:       Run endless SMS & WhatsApp Notifications using WooCommerce.
- * Version:           1.9.8.3
+ * Version:           1.9.9
  * Author:            HomeScript
  * Author URI:        https://ultimatesmsnotifications.com?utm_source=customer_websites&utm_medium=plugin_page
  * License:           GPL-2.0+
